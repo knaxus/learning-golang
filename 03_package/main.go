fc4c1f5723161cf80e2efca27d3fb68428483a2c
Ashok Dey <ashokdey100@gmail.com>
2019-09-11 18:22:10 +0530

    --done : custom packages in Go

--- 03_package/main.go
+++ 03_package/main.go
@@ -4,10 +4,16 @@ package main
 import (
 	"fmt"
 	"math"
+
+	"github.com/ashokdey/learn-golang/03_package/strutils"
 )
 
 func main() {
 	fmt.Println("Hello Packages")
 	size := 6.55
 	fmt.Println(math.Floor(size))
+
+	reverse := strutils.ReverseString("Hello World")
+
+	fmt.Println(reverse)
 }
